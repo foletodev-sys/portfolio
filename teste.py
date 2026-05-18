542bc6a4c9a848971eb374143a31cbb6c4c3d4d0
foletodev-sys <foleto.dev@gmail.com>
2026-05-17 21:00:21 -0300

    Initial commit

---
+++ teste.py
@@ -0,0 +1,5 @@
+dia = input ('Digite o dia que nasceu:')
+mes = input ('Digite o mês que nasceu: ')
+ano = input ('Digite o ano que nasceu: ')
+
+print ('Você nasceu no dia', dia, 'de', mes, 'de', ano, '.', 'Correto?' )
---
+++ teste1.py
@@ -0,0 +1,8 @@
+num1 = input ('Digite o primeiro número: ')
+num2 = input ('Digite o segundo número: ')
+
+
+
+Soma = int(num1) + int(num2) 
+
+print (Soma)
